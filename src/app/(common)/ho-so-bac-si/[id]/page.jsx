3bdbaba969c394ac9a3e0c1b2f19f376ac544d17
Thai Bao <boishiro2002@gmail.com>
2024-10-11 18:54:13 +0700

    fix display appointmentHome 11/10

--- src/app/(common)/ho-so-bac-si/[id]/page.jsx
+++ src/app/(common)/ho-so-bac-si/[id]/page.jsx
@@ -225,7 +225,7 @@ const HoSoBacSi = () => {
 
             {/* chổ này */}
 
-            {healthLogBooks.length > 0 &&
+            {
               !healthLogBooks.filter(
                 (log) =>
                   log.status.status_type === "ACCEPTED" ||
